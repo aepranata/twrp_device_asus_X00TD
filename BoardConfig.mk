bb8f1628f7d3a6ee543faa0a39dfa702ac5a5b3b
Edwin Moquete <edwinmmoquete@gmail.com>
2022-01-29 13:57:31 +0000

    X00TD: Don't blank screen on boot
    Unnecessary
    
    Change-Id: Idfb765ffd2df70cfa541c3f0e2db062c0543d254

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,7 +63,6 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS ?= 255
-TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
 TW_INCLUDE_RESETPROP := true
 
