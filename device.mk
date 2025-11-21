e6a561b514d9baf89bd1fa51d6214df59cee23a2
Aditya Eka Pranata <aepranata@gmail.com>
2025-11-21 19:43:04 +0700

    X00TD: Use Soong namespace to avoid device build conflicts
    Change-Id: Ib54c4858fc8c3f273aa0d0fdade05c04c3e77b11

--- device.mk
+++ device.mk
@@ -5,8 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/asus/ASUS_X00TD
-
 # Inherit AOSP product makefiles
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
