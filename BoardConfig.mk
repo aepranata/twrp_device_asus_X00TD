b05fe040f5b7c37c5eac934d07f1d2a718d8f19e
Aditya Eka Pranata <aepranata@gmail.com>
2025-11-25 14:12:03 +0700

    X00TD: Import kernel cmdline from CAF
    Change-Id: I0190f843612b6a4d44ca52e2c461204fb990f1df

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,3 +21,15 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x37
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=c0c4000.sdhci
+BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000
+BOARD_KERNEL_CMDLINE += androidboot.configfs=true
+BOARD_KERNEL_CMDLINE += loop.max_part=7
